829f4a07b85b13f7b4a7054b5219f5e085f6d9bc
budiliauw87 <budiliauw87@gmail.com>
2024-09-10 23:40:20 +0700

    feat adding fetch data internet using http package

--- flutter lab/fundamental/stater_flutter/lib/repository/data_source.dart
+++ flutter lab/fundamental/stater_flutter/lib/repository/data_source.dart
@@ -1,1 +1,27 @@
-class DataSource {}
+import 'dart:js_interop';
+
+import 'package:http/http.dart' as http;
+
+class DataSource {
+  final String baseUrl = 'https://restaurant-api.dicoding.dev/';
+
+  Future<http.Response> fetchRestaurants() {
+    return http.get(Uri.parse(baseUrl + 'list'));
+  }
+
+  Future<http.Response> fetchRestaurant(String id) {
+    if (id == null) throw NullRejectionException;
+    return http.get(Uri.parse(baseUrl + 'detail/' + id));
+  }
+
+  Future<http.Response> searchRestaurants(String query) {
+    if (query == null) throw NullRejectionException;
+    return http.get(Uri.parse(baseUrl + 'search?q=' + query));
+  }
+
+  Future<http.Response> addReviews(String query) {
+    if (query == null) throw NullRejectionException;
+    return http.post(Uri.parse(baseUrl + 'review'),
+        body: {'name': 'doodle', 'color': 'blue'});
+  }
+}
